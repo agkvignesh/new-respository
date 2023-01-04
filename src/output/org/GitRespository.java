6b19df8f63054310f71fd2be4ad57cce6ba836aa
agkvignesh <agkvignesh42@gmail.com>
2023-01-04 18:28:50 +0530

    https://github.com/agkvignesh/new-respository.git

---
+++ src/output/org/GitRespository.java
@@ -0,0 +1,21 @@
+package output.org;
+
+public class GitRespository {
+
+	
+		
+	public  static void studentname () {
+		System.out.println("vignesh");
+	}
+		
+		
+	public static void main(String []args) {
+		
+		GitRespository.studentname();
+		
+		
+		
+		
+		
+	}	
+}
